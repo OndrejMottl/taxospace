658bbd48b6039bacffc240e45640d9a95de4a03e
OndrejMottl <ondrej.mottl@gmail.com>
2024-02-13 16:44:26 +0100

    get_cached_file_names: Add parameter to specify directory for cached files

--- R/get_cached_file_names.R
+++ R/get_cached_file_names.R
@@ -1,6 +1,7 @@
 #' @title Get the names of cached files in the taxospace directory.
 #' @description
 #' This function retrieves the names of cached files in the taxospace directory.
+#' @param sel_dir A string indicating the directory to search for cached files.
 #' @return A character vector containing the names of the cached files.
 get_cached_file_names <- function(sel_dir) {
   list.files(
