55a8428bda8c36c5b115b80307cee02f57335674
Ondřej Mottl <60345393+OndrejMottl@users.noreply.github.com>
2025-05-02 21:01:36 +0200

    0.0.9005 (#19)

--- R/get_accepted_row.R
+++ R/get_accepted_row.R
@@ -4,27 +4,57 @@
 #' with the status "ACCEPTED".
 #' If the data source does not have a "status" column, it returns the first row.
 #' @param data_source The data source to filter
+#' @param only_accepted
+#' Logical indicating whether to filter for "ACCEPTED" status
 #' @return
 #' The first row with status "ACCEPTED" or the first row if "status" column
 #' is not present
 #' @importFrom rlang .data
-get_accepted_row <- function(data_source) {
-  res <-
-    data_source %>%
-    dplyr::slice(1)
+get_accepted_row <- function(data_source, only_accepted = TRUE) {
+  assertthat::assert_that(
+    is.data.frame(data_source),
+    msg = "data_source must be a data frame."
+  )
+
+  assertthat::assert_that(
+    nrow(data_source) > 0,
+    msg = "data_source must have at least one row."
+  )
+
+  assertthat::assert_that(
+    is.logical(only_accepted),
+    msg = "only_accepted must be a logical value."
+  )
+
+  assertthat::assert_that(
+    length(only_accepted) == 1,
+    msg = "only_accepted must be a single logical value."
+  )
+
+  res <- data_source
 
   if (
-    "status" %in% names(data_source)
+    !"status" %in% names(res)
   ) {
-    if (
-      "ACCEPTED" %in% unique(data_source$status)
-    ) {
-      res <-
-        data_source %>%
-        dplyr::filter(.data$status == "ACCEPTED") %>%
-        dplyr::slice(1)
-    }
+    res <-
+      res %>%
+      dplyr::slice(1)
+
+    return(res)
   }
+
+  if (
+    "ACCEPTED" %in% unique(data_source$status) && isTRUE(only_accepted)
+  ) {
+    res <-
+      res %>%
+      dplyr::filter(.data$status == "ACCEPTED")
+  }
+
+  res <-
+    res %>%
+    dplyr::filter(.data$confidence == max(.data$confidence)) %>%
+    dplyr::slice(1)
 
   return(res)
 }
--- tests/testthat/test-get_accepted_row.R
+++ tests/testthat/test-get_accepted_row.R
@@ -3,12 +3,21 @@ testthat::test_that("returns the first row with status 'ACCEPTED'", {
   # Create a sample data frame
   data_test <-
     data.frame(
-      status = c("REJECTED", "ACCEPTED", "PENDING"),
-      value = c(1, 2, 3)
+      status = c("REJECTED", "ACCEPTED", "ACCEPTED", "PENDING"),
+      confidence = c(0.9, 0.7, 0.8, 0.7),
+      value = c(1, 2, 3, 4)
     )
+
   result <- get_accepted_row(data_test)
 
-  testthat::expect_equal(result, data.frame(status = "ACCEPTED", value = 2))
+  expected_row <-
+    data.frame(
+      status = "ACCEPTED",
+      confidence = 0.8,
+      value = 3
+    )
+
+  testthat::expect_equal(result, expected_row)
 })
 
 # Test when 'status' column does not exist
@@ -18,7 +27,35 @@ testthat::test_that("returns the first row without status", {
     data.frame(
       value = c(1, 2, 3)
     )
+
   result <- get_accepted_row(data_without_status)
 
-  testthat::expect_equal(result, data.frame(value = 1))
+  expected_row <-
+    data.frame(
+      value = 1
+    )
+
+  testthat::expect_equal(result, expected_row)
+})
+
+# test when status is present but no accepted rows
+testthat::test_that("returns the first row without status", {
+  # Create a sample data frame
+  data_test <-
+    data.frame(
+      status = c("REJECTED", "ACCEPTED", "ACCEPTED", "PENDING"),
+      confidence = c(0.9, 0.7, 0.8, 0.7),
+      value = c(1, 2, 3, 4)
+    )
+
+  result <- get_accepted_row(data_test, only_accepted = FALSE)
+
+  expected_row <-
+    data.frame(
+      status = "REJECTED",
+      confidence = 0.9,
+      value = 1
+    )
+
+  testthat::expect_equal(result, expected_row)
 })
